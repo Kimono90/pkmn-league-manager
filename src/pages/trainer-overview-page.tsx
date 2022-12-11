327d576becb23a63b2d9e81a458dc6d5e46fb7db
Kim Bertrand <kim.bertrand@greenhousegroup.com>
2022-12-11 17:00:11 +0100

    Fix build

--- src/pages/trainer-overview-page.tsx
+++ src/pages/trainer-overview-page.tsx
@@ -1,10 +1,14 @@
-import react, { ReactElement } from 'react';
+import React, { ReactElement, useState } from 'react';
+import { Trainer } from '../models/Trainer';
 
 export function TrainerOverviewPage(): ReactElement {
+  const [trainers, setTrainers] = useState<Trainer[]>([]);
+
+  const trainerElements = trainers.map((t) => <div>{t.name}</div>)
 
   return <div>
     <h1>Registered trainers</h1>
-    <div>Trainer table</div>
+    {trainerElements}
   </div>
 
 }
